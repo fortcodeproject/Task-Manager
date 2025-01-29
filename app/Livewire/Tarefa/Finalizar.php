844d777eb707e14c393ab54a37f2e56ab8a704f0
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-01-29 13:20:06 +0100

    Finalizando tarefas

--- app/Livewire/Tarefa/Finalizar.php
+++ app/Livewire/Tarefa/Finalizar.php
@@ -2,12 +2,48 @@
 
 namespace App\Livewire\Tarefa;
 
+use App\Models\PermissaoTarefa;
+use App\Models\Tarefa;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Finalizar extends Component
 {
+    public $permissaoTarefas, $tarefas;
+    public $usuarioLogado;
+    public $permicaoUsuarioLogado = array();
+
     public function render()
     {
+        $this->usuarioLogado = Auth::user();
+        $this->tarefas = $this->buscarTarefas();
+        $this->permissaoTarefas = $this->buscarPermissoesTarefas();
         return view('livewire.tarefa.finalizar');
+    }
+
+    public function buscarPermissoesTarefas()
+    {
+        return PermissaoTarefa::all();
+    }
+
+    public function buscarTarefas()
+    {
+        return Tarefa::where("realizador", $this->usuarioLogado->id)
+            ->where("estado", "em andamento")
+            ->get();
+    }
+
+    public function finalizarTarefa($idTarefa)
+    {
+        Tarefa::where("id", $idTarefa)->update([
+            'realizador' => Auth::user()->id,
+            'estado' => "finalizado",
+        ]);
+
+        $this->dispatch("alerta", [
+            "icon" => "success",
+            "mensagem" => "Tarefa Finalizada",
+            "tempo" => 4000,
+        ]);
     }
 }
