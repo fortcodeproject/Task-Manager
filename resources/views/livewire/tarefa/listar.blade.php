7a019f99bcbd4f5642bb1d726db7aff3e39dc00c
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-01-29 15:00:30 +0100

    Botão realizar somente em Usuários normais

--- resources/views/livewire/tarefa/listar.blade.php
+++ resources/views/livewire/tarefa/listar.blade.php
@@ -26,11 +26,7 @@
                                     <th>Usuário Específico</th>
                                     <th>Situação</th>
                                     <th>Criador</th>
-
-                                    @if ($usuarioLogado->id_acesso == 2)
-                                        <th>Realizador</th>
-                                    @endif
-
+                                    <th>Realizador</th>
                                     <th style="width: 10%">Acção</th>
                                 </tr>
                             </thead>
@@ -40,53 +36,57 @@
                                         $tarefa = $this->buscarTodasTarefas($permissao->id_tarefa);
                                     @endphp
 
-                                        @if ($permissao->leitura == 'permitido' && $permissao->id_usuario == $usuarioLogado->id)
-                                            <tr>
-                                                <td>{{ $tarefa->id }}</td>
-                                                <td style="white-space: nowrap">{{ $tarefa->titulo }}</td>
-                                                <td>{{ $tarefa->descricao }}</td>
-                                                <td>{{ ucwords($tarefa->estado) }}</td>
-                                                <td style="white-space: nowrap">
-                                                    @if ($tarefa->usuario_especifico != null)
-                                                        {{ ucwords($tarefa->buscarUsuarioEspecifico->name) }}
+                                    @if ($permissao->leitura == 'permitido' && $permissao->id_usuario == $usuarioLogado->id)
+                                        <tr>
+                                            <td>{{ $tarefa->id }}</td>
+                                            <td style="white-space: nowrap">{{ $tarefa->titulo }}</td>
+                                            <td>{{ $tarefa->descricao }}</td>
+                                            <td style="white-space: nowrap">{{ ucwords($tarefa->estado) }}</td>
+                                            <td style="white-space: nowrap">
+                                                @if ($tarefa->usuario_especifico != null)
+                                                    {{ ucwords($tarefa->buscarUsuarioEspecifico->name) }}
+                                                @else
+                                                    Vazio
+                                                @endif
+                                            </td>
+                                            <td>{{ ucwords($tarefa->situacao) }}</td>
+                                            <td style="white-space: nowrap">
+                                                {{ ucwords($tarefa->buscarUsuarioCriador->name) }}
+                                            </td>
+
+
+                                            <td>
+                                                @if ($tarefa->realizador != null)
+                                                    {{ ucwords($tarefa->buscarUsuarioRealizador->name) }}
+                                                @else
+                                                    @if ($usuarioLogado->id_acesso == 2)
+                                                        <button type="button"
+                                                            wire:click.prevent='realizarTarefa({{ $tarefa->id }})'
+                                                            class="btn btn-success btn-primary btn-lg">
+                                                            Realizar
+                                                        </button>
                                                     @else
                                                         Vazio
                                                     @endif
-                                                </td>
-                                                <td>{{ ucwords($tarefa->situacao) }}</td>
-                                                <td style="white-space: nowrap">
-                                                    {{ ucwords($tarefa->buscarUsuarioCriador->name) }}
-                                                </td>
-
-                                                @if ($usuarioLogado->id_acesso == 2)
-                                                    <td>
-                                                        @if ($tarefa->realizador != null)
-                                                             {{ ucwords($tarefa->buscarUsuarioRealizador->name) }}
-                                                        @else
-                                                            <button type="button" wire:click.prevent='realizarTarefa({{$tarefa->id}})'
-                                                                class="btn btn-success btn-primary btn-lg">
-                                                                Realizar
-                                                            </button>
-                                                        @endif
-                                                    </td>
                                                 @endif
+                                            </td>
 
-                                                <td>
-                                                    <div class="form-button-action">
-                                                        <button type="button" data-bs-toggle="tooltip" title=""
-                                                            class="btn btn-link btn-primary btn-lg"
-                                                            data-original-title="Edit Task">
-                                                            <i class="fa fa-edit"></i>
-                                                        </button>
-                                                        <button type="button" data-bs-toggle="tooltip" title=""
-                                                            class="btn btn-link btn-danger"
-                                                            data-original-title="Remove">
-                                                            <i class="fa fa-times"></i>
-                                                        </button>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        @endif
+
+                                            <td>
+                                                <div class="form-button-action">
+                                                    <button type="button" data-bs-toggle="tooltip" title=""
+                                                        class="btn btn-link btn-primary btn-lg"
+                                                        data-original-title="Edit Task">
+                                                        <i class="fa fa-edit"></i>
+                                                    </button>
+                                                    <button type="button" data-bs-toggle="tooltip" title=""
+                                                        class="btn btn-link btn-danger" data-original-title="Remove">
+                                                        <i class="fa fa-times"></i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endif
                                 @endforeach
                             </tbody>
                         </table>
