1b724eab1de95a432c88b0fba7425c498cc38e7b
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-01-29 02:17:30 +0100

    Alterando senha com sucesso

--- app/Livewire/Usuario/AlterarSenha.php
+++ app/Livewire/Usuario/AlterarSenha.php
@@ -12,6 +12,21 @@ class AlterarSenha extends Component
     public $usuario, $senhaActual, $senhaNova, $senhaConfirmacao;
     public $msgSenhaActual, $msgSenhaNova, $msgSenhaConfirmacao;
 
+    protected $rules = [
+        "senhaActual" => "required|min:6",
+        "senhaNova" => "required|min:6",
+        "senhaConfirmacao" => "required|min:6",
+    ];
+
+    protected $messages = [
+        "senhaActual.required" => "Campo obrigatório",
+        "senhaActual.min" => "A senha deve conter no mínimo 6 caracteres",
+        "senhaNova.required" => "Campo obrigatório",
+        "senhaNova.min" => "A senha deve conter no mínimo 6 caracteres",
+        "senhaConfirmacao.required" => "Campo obrigatório",
+        "senhaConfirmacao.min" => "A senha deve conter no mínimo 6 caracteres",
+    ];
+
     public function render()
     {
         $idUsuario = Auth::user()->id;
@@ -20,33 +35,53 @@ class AlterarSenha extends Component
             ->layout("components.layouts.app");
     }
 
-    public function verificarSenhaActual(){
-        if($this->senhaActual != null && !Hash::check($this->senhaActual, $this->usuario->password)){
+    public function verificarSenhaActual()
+    {
+        if ($this->senhaActual != null && !Hash::check($this->senhaActual, $this->usuario->password)) {
             $this->msgSenhaActual = "Senha actual errada";
-        }else{
+        } else {
             $this->msgSenhaActual = null;
         }
     }
 
-    public function verificarSenhaNova(){
-        if($this->senhaNova != null && Hash::check($this->senhaNova, $this->usuario->password)){
+    public function verificarSenhaNova()
+    {
+        if ($this->senhaNova != null && Hash::check($this->senhaNova, $this->usuario->password)) {
             $this->msgSenhaNova = "Senha nova deve ser diferente da actual";
-        }else{
+        } else {
             $this->msgSenhaNova = null;
         }
     }
 
-    public function verificarSenhaConfirmacao(){
-        if($this->senhaNova == null){
+    public function verificarSenhaConfirmacao()
+    {
+        if ($this->senhaNova == null && $this->senhaConfirmacao != null) {
             $this->msgSenhaConfirmacao = "Senha não deve estar vazio";
-        }else if($this->senhaNova != $this->senhaConfirmacao){
+        } else if ($this->senhaNova != $this->senhaConfirmacao) {
             $this->msgSenhaConfirmacao = "Senha nova e de confirmação devem ser iguais";
-        }else{
+        } else {
             $this->msgSenhaConfirmacao = null;
         }
     }
 
-    public function alterar(){
-        dd("ok");
+    public function alterar()
+    {
+        if ($this->msgSenhaActual == null && $this->msgSenhaNova == null && $this->msgSenhaConfirmacao == null) {
+            $this->validate();
+            User::where("id", $this->usuario->id)->update([
+                'password' => Hash::make($this->senhaNova),
+            ]);
+            $this->dispatch("alerta", [
+                "icon" => "success",
+                "mensagem" => "Senha alterada com sucesso",
+                "tempo" => 4000,
+            ]);
+            $this->limparCampos();
+        }
+    }
+
+    public function limparCampos()
+    {
+        $this->senhaActual = $this->senhaNova = $this->senhaConfirmacao = null;
     }
 }
