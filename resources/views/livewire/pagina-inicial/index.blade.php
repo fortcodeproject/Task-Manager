36b6e153aaf288dd02b6987cd1cc2fc868b19213
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-01-29 13:44:56 +0100

    Somente o SuperAdmin pode ver total de usuarios na pag. Inicial

--- resources/views/livewire/pagina-inicial/index.blade.php
+++ resources/views/livewire/pagina-inicial/index.blade.php
@@ -1,4 +1,9 @@
 <div class="container ">
+    @php
+        use Illuminate\Support\Facades\Auth;
+        $usuario = Auth::user();
+    @endphp
+    
     <div class="page-inner">
         <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
             <div>
@@ -7,25 +12,28 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-6 col-md-3">
-                <div class="card card-stats card-round">
-                    <div class="card-body">
-                        <div class="row align-items-center">
-                            <div class="col-icon">
-                                <div class="icon-big text-center icon-primary bubble-shadow-small">
-                                    <i class="fas fa-users"></i>
+            @if ($usuario->id_acesso == 1)
+                <div class="col-sm-6 col-md-3">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-icon">
+                                    <div class="icon-big text-center icon-primary bubble-shadow-small">
+                                        <i class="fas fa-users"></i>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col col-stats ms-3 ms-sm-0">
-                                <div class="numbers">
-                                    <p class="card-category">Usuários</p>
-                                    <h4 class="card-title">{{ count($usuarios) }}</h4>
+                                <div class="col col-stats ms-3 ms-sm-0">
+                                    <div class="numbers">
+                                        <p class="card-category">Usuários</p>
+                                        <h4 class="card-title">{{ count($usuarios) }}</h4>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endif
+
             <div class="col-sm-6 col-md-3">
                 <div class="card card-stats card-round">
                     <div class="card-body">
@@ -518,6 +526,6 @@
                     </div>
                 </div>
             </div>
-        </div>--}}
+        </div> --}}
     </div>
 </div>
